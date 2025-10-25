49a59aef224451321ca914bfd752c1c21d98fb99
Gustavo <149015908+Gustavoogalo@users.noreply.github.com>
2025-10-25 06:37:59 -0300

    feat - spawn de inimigos

--- Assets/Scripts/Inimigo/EnemyManager.cs
+++ Assets/Scripts/Inimigo/EnemyManager.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using Camera;
+using Helpers;
+using Player;
 using Stands;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,33 +12,83 @@ namespace Inimigo
     [RequireComponent(typeof(NavMeshAgent))]
     public class EnemyManager : MonoBehaviour
     {
+        public enum EnemyType
+        {
+            TypeA,
+            TypeB
+        }
+
+        public EnemyType Type { get; private set; }
+
+        public event Action<EnemyManager> OnReturnToPool;
+        private EnemySpawner spawner;
+
         private NavMeshAgent agent;
         [SerializeField] private float velocity = 5f;
         [SerializeField] private Transform target;
         [SerializeField] private float offset;
 
-        [SerializeField] private StandsResourceSpawner resourceSpawner;
+        private Animator _animator;
+        private HealthSystem _healthSystem;
+        [SerializeField] private GameObject _vfxPrefab;
+        private Camera_ShakeController cameraShake;
+
+        private StandsResourceSpawner resourceSpawner;
         [SerializeField] private TriggerCheck colliderCheck;
+
         private void Awake()
         {
-            agent = GetComponent<NavMeshAgent>();
         }
 
-        private void OnEnable()
+        public void Setup(EnemySpawner owner, EnemyType type, Transform targetStand)
         {
+            spawner = owner;
+            Type = type;
+            agent = GetComponent<NavMeshAgent>();
+            target = targetStand;
+            if (colliderCheck == null)
+            {
+                colliderCheck = GetComponentInChildren<TriggerCheck>();
+            }
+
+            if (_healthSystem == null)
+            {
+                _healthSystem = GetComponent<HealthSystem>();
+            }
+
+            if (_animator == null)
+            {
+                _animator = GetComponent<Animator>();
+            }
+
+
             colliderCheck.EnteredTrigger += EnteredInStand;
+            _healthSystem.OnDeath += OnDeathHandler;
+
+            gameObject.SetActive(false);
+        }
+
+        public void Activate(Vector3 spawnPosition)
+        {
+            ResetState();
+            transform.position = spawnPosition;
+            agent.enabled = true;
+            agent.isStopped = false;
+            if (cameraShake == null)
+            {
+                cameraShake = ServiceLocator.Get<Camera_ShakeController>();
+            }
+
+            gameObject.SetActive(true);
+
+            if (target == null) return;
+            var targetPos = new Vector3(target.position.x - offset, 0f, target.position.z - offset);
+            agent.SetDestination(targetPos);
+            agent.speed = velocity;
         }
 
         private void OnDisable()
         {
-            colliderCheck.EnteredTrigger -= EnteredInStand;
-        }
-
-        private void Start()
-        {
-            if (target == null) return;
-            var targetPos = new Vector3(target.position.x - offset, 0f, target.position.z - offset);
-            agent.SetDestination(targetPos);
         }
 
         private void EnteredInStand(Collider other)
@@ -47,19 +101,57 @@ namespace Inimigo
                     resourceSpawner = other.GetComponent<StandsResourceSpawner>();
                 }
 
-                //inicia animacao de magia...
                 resourceSpawner.PauseCraft();
             }
         }
-        
 
-        public void Death()
+        private void OnDeathHandler(HealthSystem health)
+        {
+            _healthSystem.OnDeath -= OnDeathHandler;
+            StartCoroutine(Dying());
+        }
+
+        IEnumerator Dying()
         {
             agent.isStopped = true;
+            agent.enabled = false;
+
+            _animator.Play("EnemyDeath");
+
+            yield return new WaitForSeconds(0.2f);
+
+            cameraShake.ShakeCamera(5f, 0.5f, 0.1f);
+
+            yield return new WaitForSeconds(0.1f);
+
+            ReturnToPoolLogic();
+        }
+
+        public void ReturnToPoolLogic()
+        {
             if (resourceSpawner != null)
             {
                 resourceSpawner.ResumeCraft();
+                resourceSpawner = null;
             }
+
+            _healthSystem.Revive();
+            _healthSystem.OnDeath += OnDeathHandler;
+            gameObject.SetActive(false);
+            OnReturnToPool?.Invoke(this);
+        }
+
+        private void ResetState()
+        {
+            _healthSystem.Heal(_healthSystem.MaxHealth);
+
+            if (_animator != null)
+            {
+                _animator.Play("Idle");
+            }
+
+            agent.enabled = true;
+            resourceSpawner = null;
         }
     }
 }
---
+++ Assets/Scripts/Inimigo/EnemySpawner.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Inimigo
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [Header("Pool Settings")]
+        [SerializeField] private int poolSize = 15;
+        [SerializeField] private Transform spawnPoint;
+
+        [Header("Enemy Prefabs")]
+        [SerializeField] private EnemyManager enemyTypeAPrefab;
+        [SerializeField] private EnemyManager enemyTypeBPrefab;
+
+        public enum EnemyTypeToSpawn { TypeA, TypeB }
+        [Header("Spawn Control")]
+        public EnemyTypeToSpawn currentSpawnType = EnemyTypeToSpawn.TypeA;
+
+        [Header("Spawn Timer")]
+        [SerializeField] private float timeBetweenSpawns = 5f;
+        private float spawnCounter;
+
+        public enum SpawnerState { Active, Paused }
+        private SpawnerState currentState = SpawnerState.Active;
+
+        private List<EnemyManager> availableEnemies = new List<EnemyManager>();
+        private List<EnemyManager> activeEnemies = new List<EnemyManager>();
+        
+        [SerializeField] private List<Transform> targetStands = new List<Transform>();
+
+        private void Awake()
+        {
+            if (spawnPoint == null)
+            {
+                spawnPoint = transform;
+            }
+            InitializePool();
+            spawnCounter = timeBetweenSpawns;
+        }
+
+        private void InitializePool()
+        {
+            int countA = poolSize / 2;
+            int countB = poolSize - countA; 
+
+            for (int i = 0; i < countA; i++)
+            {
+                CreateNewEnemy(enemyTypeAPrefab, EnemyManager.EnemyType.TypeA);
+            }
+
+            for (int i = 0; i < countB; i++)
+            {
+                CreateNewEnemy(enemyTypeBPrefab, EnemyManager.EnemyType.TypeB);
+            }
+        }
+
+        private void CreateNewEnemy(EnemyManager prefab, EnemyManager.EnemyType type)
+        {
+            if (prefab == null) return;
+
+            EnemyManager newEnemy = Instantiate(prefab, transform);
+            var randomStand = Random.Range(0, targetStands.Count);
+            newEnemy.Setup(this, type, targetStands[randomStand]);
+            newEnemy.OnReturnToPool += ReturnEnemyToPool;
+            
+            availableEnemies.Add(newEnemy);
+        }
+
+        private void Update()
+        {
+            if (currentState == SpawnerState.Paused)
+            {
+                return;
+            }
+
+            if (spawnCounter > 0)
+            {
+                spawnCounter -= Time.deltaTime;
+            }
+            else
+            {
+                AttemptToSpawnEnemy();
+                spawnCounter = timeBetweenSpawns;
+            }
+        }
+
+        private void AttemptToSpawnEnemy()
+        {
+            EnemyManager spawned = SpawnEnemy();
+            if (spawned == null)
+            {
+                spawnCounter = 0;
+            }
+        }
+
+        public EnemyManager SpawnEnemy()
+        {
+            EnemyManager.EnemyType targetType = currentSpawnType == EnemyTypeToSpawn.TypeA ? 
+                                                    EnemyManager.EnemyType.TypeA : 
+                                                    EnemyManager.EnemyType.TypeB;
+            
+            EnemyManager enemyToSpawn = availableEnemies.Find(e => e.Type == targetType);
+
+            if (enemyToSpawn != null)
+            {
+                availableEnemies.Remove(enemyToSpawn);
+                activeEnemies.Add(enemyToSpawn);
+                
+                enemyToSpawn.Activate(spawnPoint.position);
+                
+                return enemyToSpawn;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        private void ReturnEnemyToPool(EnemyManager enemy)
+        {
+            if (activeEnemies.Contains(enemy))
+            {
+                activeEnemies.Remove(enemy);
+                availableEnemies.Add(enemy);
+                
+                if (spawnCounter <= 0)
+                {
+                    spawnCounter = 0;
+                }
+            }
+        }
+
+        public void PauseSpawner()
+        {
+            currentState = SpawnerState.Paused;
+        }
+
+        public void ResumeSpawner()
+        {
+            if (currentState == SpawnerState.Paused)
+            {
+                currentState = SpawnerState.Active;
+            }
+        }
+    }
+}
